106c8775881e591ac382374510f3bc4e37f53f45
marcus-elia <mselia@uvm.edu>
2023-09-06 09:32:51 -0400

    Added separate memory and time optimized modes. 30 mins vs 30 seconds in LynchburgVA

--- main_scripts/map_geojson_to_tiles.py
+++ main_scripts/map_geojson_to_tiles.py
@@ -30,6 +30,12 @@ def map_shapely_polygon_into_tile(tile, shapely_polygon_latlon, manual_offset=(0
     else:
         return shapely.Polygon()
 
+def get_overlapping_tiles(shapely_polygon_utm, zone):
+    x_min, y_min, x_max, y_max = shapely_polygon_utm.bounds
+    min_tile = TileID(x_min, y_min, zone)
+    max_tile = TileID(x_max, y_max, zone)
+    return (min_tile.i, min_tile.j, max_tile.i, max_tile.j)
+
 def main():
     parser = argparse.ArgumentParser(description="Map geojson polygons into tiles.")
     parser.add_argument("-i", "--input", required=True, help="Path to input geojson file")  
@@ -40,6 +46,9 @@ def main():
     parser.add_argument("--ne", required=True, help='NE corner formatted as "lat,lon" or "lat, lon"')
     parser.add_argument("--offset_x", required=False, type=int, default=0, help='Offset x coord of each point')
     parser.add_argument("--offset_y", required=False, type=int, default=0, help='Offset y coord of each point')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--time", action='store_true', help='Optimize for time')
+    group.add_argument("--memory", action='store_true', help='Optimize for memory usage')
 
     args = parser.parse_args()
 
@@ -73,58 +82,145 @@ def main():
     geojson_contents = geojson.loads(f.read())
     f.close()
 
-    # Intersect every geometry from the geojson with every tile
-    num_completed = 0
-    start_time = time.time()
-    for i in range(min_i, max_i + 1):
-        for j in range(min_j, max_j + 1):
-            current_tile = TileID.tile_indices_to_object(i, j, tile_min.zone)
-            tile_union = shapely.Polygon()
-            for geojson_multipolygon in geojson_contents['features']:
-                shapely_polygons = geojson_multipoly_to_shapely(geojson_multipolygon.geometry.coordinates)
-                for shapely_polygon_latlon in shapely_polygons:
-                    clipped_polygon = map_shapely_polygon_into_tile(current_tile, shapely_polygon_latlon, manual_offset=(args.offset_x, args.offset_y))
-                    tile_union = tile_union.union(clipped_polygon)
-            # Once every polygon has been intersected with the tile, overwrite the tile's geojson file
+    # When you are concerned about running out of RAM, we do the slow naive thing. For each tile, intersect
+    # every polygon with it and then immediately write to that file. Nothing gets stored.
+    if args.memory:
+        # Intersect every geometry from the geojson with every tile
+        num_completed = 0
+        start_time = time.time()
+        for i in range(min_i, max_i + 1):
+            for j in range(min_j, max_j + 1):
+                current_tile = TileID.tile_indices_to_object(i, j, tile_min.zone)
+                tile_union = shapely.Polygon()
+                for geojson_multipolygon in geojson_contents['features']:
+                    shapely_polygons = geojson_multipoly_to_shapely(geojson_multipolygon.geometry.coordinates)
+                    for shapely_polygon_latlon in shapely_polygons:
+                        clipped_polygon = map_shapely_polygon_into_tile(current_tile, shapely_polygon_latlon, manual_offset=(args.offset_x, args.offset_y))
+                        tile_union = tile_union.union(clipped_polygon)
+                # Once every polygon has been intersected with the tile, overwrite the tile's geojson file
 
-            # Convert the multipolygon from shapely to geojson
-            if type(tile_union) == shapely.geometry.multipolygon.MultiPolygon:
-                geojson_tile_union = shapely_multipolygon_to_geojson(tile_union)
-            elif type(tile_union) == shapely.geometry.polygon.Polygon:
-                geojson_tile_union = shapely_polygon_to_geojson(tile_union)
-            features = [geojson.Feature(geometry=geojson_tile_union)]
+                # Convert the multipolygon from shapely to geojson
+                if type(tile_union) == shapely.geometry.multipolygon.MultiPolygon:
+                    geojson_tile_union = shapely_multipolygon_to_geojson(tile_union)
+                elif type(tile_union) == shapely.geometry.polygon.Polygon:
+                    geojson_tile_union = shapely_polygon_to_geojson(tile_union)
+                features = [geojson.Feature(geometry=geojson_tile_union)]
 
-            # Create the tile's directory, in case it doesn't exist yet
-            full_path = os.path.join(city_directory, "%d_%d_%d" % (i, j, tile_min.zone))
-            p = subprocess.Popen(['mkdir', full_path], shell=True)
-            p.communicate()
+                # Create the tile's directory, in case it doesn't exist yet
+                full_path = os.path.join(city_directory, "%d_%d_%d" % (i, j, tile_min.zone))
+                p = subprocess.Popen(['mkdir', full_path], shell=True)
+                p.communicate()
 
-            # Dump the geojson object into a string
-            dump = geojson.dumps(geojson.FeatureCollection(features=features, crs=geojson_crs))
+                # Dump the geojson object into a string
+                dump = geojson.dumps(geojson.FeatureCollection(features=features, crs=geojson_crs))
 
-            # Finally, write to the file
-            full_path = os.path.join(full_path, args.output_filename)
-            f = open(full_path, 'w')
-            f.write(dump)
-            f.close()
+                # Finally, write to the file
+                full_path = os.path.join(full_path, args.output_filename)
+                f = open(full_path, 'w')
+                f.write(dump)
+                f.close()
 
-            # Log the status
-            num_completed += 1
-            percent_complete = float(num_completed) / float(num_tiles) * 100
-            time_elapsed = int(time.time() - start_time)
-            time_remaining = int(time_elapsed * (100 - percent_complete) / percent_complete)
-            if time_elapsed < 120:
-                time_string = "%d seconds, %d seconds remaining" % (time_elapsed, time_remaining)
-            else:
-                time_elapsed = int(time_elapsed / 60)
-                time_remaining = int(time_remaining / 60)
+                # Log the status
+                num_completed += 1
+                percent_complete = float(num_completed) / float(num_tiles) * 100
+                time_elapsed = int(time.time() - start_time)
+                time_remaining = int(time_elapsed * (100 - percent_complete) / percent_complete)
                 if time_elapsed < 120:
-                    time_string = "%d minutes, %d minutes remaining" % (time_elapsed, time_remaining)
+                    time_string = "%d seconds, %d seconds remaining" % (time_elapsed, time_remaining)
                 else:
                     time_elapsed = int(time_elapsed / 60)
                     time_remaining = int(time_remaining / 60)
-                    time_string = "%d hours, %d hours remaining" % (time_elapsed, time_remaining)
-            print("Completed %d/%d tiles (%.1f percent) in %s" % (num_completed, num_tiles, percent_complete, time_string))
+                    if time_elapsed < 120:
+                        time_string = "%d minutes, %d minutes remaining" % (time_elapsed, time_remaining)
+                    else:
+                        time_elapsed = int(time_elapsed / 60)
+                        time_remaining = int(time_remaining / 60)
+                        time_string = "%d hours, %d hours remaining" % (time_elapsed, time_remaining)
+                print("Completed %d/%d tiles (%.1f percent) in %s" % (num_completed, num_tiles, percent_complete, time_string))
+        print("Stored %d %s files in %s." % (num_tiles, args.output_filename, full_path))
+
+    # If we have plenty of RAM and want things to run faster, we only intersect each polygon with the
+    # tiles that actually overlap with it. This requires storing a map from each tile to its polygon.
+    elif args.time:
+        # Start by mapping every tile to an empty polygon
+        tile_to_polygon_map = {}
+        print("Initializing an empty polygon for all %d tiles." % (num_tiles))
+        for i in range(min_i, max_i + 1):
+            for j in range(min_j, max_j + 1):
+                tile_to_polygon_map[(i, j)] = shapely.Polygon()
+
+        # Collect info for logging
+        start_time = time.time()
+        num_polygons = num_polygons_in_geojson_file(geojson_contents)
+        num_completed = 0
+
+        # Now iterate over every polygon in the geojson, intersecting only with relevant tiles
+        for geojson_multipolygon in geojson_contents['features']:
+            shapely_polygons = geojson_multipoly_to_shapely(geojson_multipolygon.geometry.coordinates)
+            for shapely_polygon_latlon in shapely_polygons:
+                shapely_polygon_utm = poly_latlon_to_utm(shapely_polygon_latlon, offset=(args.offset_x, args.offset_y))
+
+                # With the polygon in UTM, determine which tiles overlap with its bbox.
+                # Also, make sure it doesn't go beyond the user-specified tile area.
+                bbox_i_min, bbox_j_min, bbox_i_max, bbox_j_max = get_overlapping_tiles(shapely_polygon_utm, tile_min.zone)
+                bbox_i_min = max(bbox_i_min, min_i)
+                bbox_j_min = max(bbox_j_min, min_j)
+                bbox_i_max = min(bbox_i_max, max_i)
+                bbox_j_max = min(bbox_j_max, max_j)
+
+                # Intersect it with each of those tiles
+                for i in range(bbox_i_min, bbox_i_max + 1):
+                    for j in range(bbox_j_min, bbox_j_max + 1):
+                        current_tile = TileID.tile_indices_to_object(i, j, tile_min.zone)
+                        clipped_poly = current_tile.polygon().intersection(shapely_polygon_utm)
+                        if not clipped_poly.is_simple:
+                            continue
+
+                        tile_to_polygon_map[(i, j)] = tile_to_polygon_map[(i, j)].union(clipped_poly)
+
+                # Log the status
+                num_completed += 1
+                percent_complete = float(num_completed) / float(num_polygons) * 100
+                time_elapsed = int(time.time() - start_time)
+                time_remaining = int(time_elapsed * (100 - percent_complete) / percent_complete)
+                if time_elapsed < 120:
+                    time_string = "%d seconds, %d seconds remaining" % (time_elapsed, time_remaining)
+                else:
+                    time_elapsed = int(time_elapsed / 60)
+                    time_remaining = int(time_remaining / 60)
+                    if time_elapsed < 120:
+                        time_string = "%d minutes, %d minutes remaining" % (time_elapsed, time_remaining)
+                    else:
+                        time_elapsed = int(time_elapsed / 60)
+                        time_remaining = int(time_remaining / 60)
+                        time_string = "%d hours, %d hours remaining" % (time_elapsed, time_remaining)
+                print("Completed %d/%d polygons (%.1f percent) in %s" % (num_completed, num_polygons, percent_complete, time_string))
+
+        # The tile to polygon map is complete. Write each tile's geojson file.
+        for i in range(min_i, max_i + 1):
+            for j in range(min_j, max_j + 1):
+                tile_union = tile_to_polygon_map[(i, j)]
+                # Convert the multipolygon from shapely to geojson
+                if type(tile_union) == shapely.geometry.multipolygon.MultiPolygon:
+                    geojson_tile_union = shapely_multipolygon_to_geojson(tile_union)
+                elif type(tile_union) == shapely.geometry.polygon.Polygon:
+                    geojson_tile_union = shapely_polygon_to_geojson(tile_union)
+                features = [geojson.Feature(geometry=geojson_tile_union)]
+
+                # Create the tile's directory, in case it doesn't exist yet
+                full_path = os.path.join(city_directory, "%d_%d_%d" % (i, j, tile_min.zone))
+                p = subprocess.Popen(['mkdir', full_path], shell=True)
+                p.communicate()
+
+                # Dump the geojson object into a string
+                dump = geojson.dumps(geojson.FeatureCollection(features=features, crs=geojson_crs))
+
+                # Finally, write to the file
+                full_path = os.path.join(full_path, args.output_filename)
+                f = open(full_path, 'w')
+                f.write(dump)
+                f.close()
+        print("Stored %d %s files in %s." % (num_tiles, args.output_filename, full_path))
 
 if __name__ == "__main__":
     main()

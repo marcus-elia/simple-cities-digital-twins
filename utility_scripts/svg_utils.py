fd2fff667efceb12a194e0a799601a64ba707dbd
marcus-elia <mselia@uvm.edu>
2023-09-08 11:28:20 -0400

    Get basic SVG writing to work

---
+++ utility_scripts/svg_utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import shapely
+
+from tile_id import *
+
+def create_tile_svg(tile, color_polygons_pairs, output_filepath):
+    f = open(output_filepath, 'w')
+
+    # Determine the bounds and write the header
+    x_min, y_min = tile.sw_corner()
+    x_max = x_min + TileID.TILE_SIZE
+    y_max = y_min + TileID.TILE_SIZE
+    #f.write('<svg viewBox="%d %d %d %d">\n' % (x_min, y_min, x_max, y_max))
+    f.write('<svg viewBox="%d %d %d %d">\n' % (0, 0, TileID.TILE_SIZE, TileID.TILE_SIZE))
+
+    for color, shapely_polygons in color_polygons_pairs:
+        for shapely_polygon in shapely_polygons:
+            f.write('\t<path\n')
+            f.write('\t\tfill-rule="evenodd"\n')
+            f.write('\t\tstroke="%s"\n' % (color))
+            f.write('\t\tfill="%s"\n' % (color))
+            vertices_string = '\t\td="M '
+            for x,y in shapely_polygon.exterior.coords:
+                vertices_string += '%f,%f ' % (y - x_min, x - y_min)
+            vertices_string += 'z\n'
+            for hole in shapely_polygon.interiors:
+                vertices_string += 'M '
+                for x,y in hole.coords:
+                    vertices_string += '%f,%f ' % (y - x_min, x - y_min)
+                vertices_string += 'z\n'
+            vertices_string += '" />\n'
+            f.write(vertices_string)
+
+    f.write('</svg>')
